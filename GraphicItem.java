6987140ea3b62f676541071368b0c08d5b5dcabb
payanthe <payanthe@gmail.com>
2016-12-16 17:19:40 +0330

    Starting Project

--- GraphicItem.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by parsa on 11/30/2016.
- */
-public class GraphicItem extends game {
-}
--- narenjak.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by parsa on 12/10/2016.
- */
-public class narenjak extends Bombs {
-    private int[] distroyArea;
-    private int destroyStrange;
-
-}
