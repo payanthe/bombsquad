03ad6f6ea3a1d6e8f2b623aa969c51bb91e7ea8f
Unknown <masoumzadegan.erfan@gmail.com>
2016-12-25 16:32:33 +0330

    game

--- game.java
+++ game.java
@@ -1,12 +1,133 @@
-/**
- * Created by parsa on 11/30/2016.
- */
-public class game {
-    public Players turn() {
-        return null;
+import java.util.Random;
+
+
+public class Game {
+    int[][] plan;
+    Random rand = new Random();
+    int taas;
+    final int UP=1;
+    final int RIGHT=2;
+    final int DOWN=3;
+    final int LEFT=4;
+
+    Player p1;
+    Player p2;
+    private int turn;
+
+
+    Game(){
+        p1 = new Player();
+        p2 = new Player();
+        this.plan=new int[20][20];
+
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                this.plan[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            plan[i][0] = 0;
+            plan[0][i] = 0;
+            plan[19][i] = 0;
+            plan[i][19] = 0;
+        }
+
+        for (int i = 3; i <= 16; i++) {
+            plan[i][3] = -600;
+            plan[i][4] = -600;
+            plan[i][7] = -600;
+            plan[i][8] = -600;
+            plan[i][11] = -600;
+            plan[i][12] = -600;
+            plan[i][15] = -600;
+            plan[i][16] = -600;
+            p1.posX = 18;
+            p1.posY = 2;
+            p2.posX = 2;
+            p2.posY = 18;
+            p1.controller=1;
+            p2.controller=2;
+
+
+
+        }
+
+
     }
 
-    public boolean winner(Players p) {
+
+    {
+
+
+
+    }
+    public void masterGame(){
+        int randp1=1+ (int)(Math.random() * ((6 - 1) + 1));
+        int randp2=1+ (int)(Math.random() * ((6 - 1) + 1));
+        if (p1.turnAccess)
+        p1.moveCounter=randp1;
+
+
+    }
+
+    public void turndo(){
+        p1.checkHouse();
+        if (this.looser(p1)){}
+        if (this.looser(p2)){}
+
+    }
+
+    public void turnOrg()
+    {
+        if (turn==1){
+            if (p2.turnAccess==true){
+                turn=2;
+            }
+            else {
+                turn=1;
+            }
+        }
+        else {
+            if (p1.turnAccess==true){
+                turn=1;
+            }
+            else {
+                turn=2;
+            }
+        }
+    }
+
+    public void diceCount(){
+        taas= rand.nextInt(6)+1;
+
+    }
+
+    public void bombMaker(){
+        while (true) {
+            int randX = rand.nextInt(20) + 0;
+            int randY = rand.nextInt(20) + 0;
+            int randBomb = (rand.nextInt(4) + 1)*10;
+            if (plan[randX][randY]==0){
+                plan[randX][randY]=randBomb;
+                break;
+            }
+        }
+    }
+
+
+    public boolean winner(Player p) {
+        if(p.life<=0){
+            return false;
+        }
+        return true;
+    }
+    public boolean looser(Player p){
+        if(p.life<=0){
+            return true;
+        }
         return false;
     }
 }
+
--- narenjak.java
+++ narenjak.java
@@ -1,8 +1,66 @@
-/**
- * Created by parsa on 12/10/2016.
- */
-public class narenjak extends Bombs {
-    private int[] distroyArea;
-    private int destroyStrange;
+
+public class Narenjak extends Bombs {
+//    private int bombNum=20;
+
+    final int NARENJAK = 20;
+
+
+    private int power=200;
+
+    public Narenjak(int Y, int X, int direct) {
+        super.bombX=X;
+        super.bombY=Y;
+        super.direct=direct;
+    }
+
+    public void destroy() {
+
+        switch (direct){
+            case 1:
+                if (super.bombX<20 && super.bombY+3>=0 ) {
+                    if (p1.posX==super.bombX && p1.posY==super.bombY+3 ){
+                        p1.life-=power;
+                    }
+                    if (p2.posX==super.bombX && p2.posY==super.bombY+3 ){
+                        p2.life-=power;
+                    }
+                }
+            case 2:
+                if (super.bombX+3<20 && super.bombY>=0 ){
+                    if (p1.posX==super.bombX+3 && p1.posY==super.bombY ){
+                        p1.life-=power;
+                    }
+                    if (p2.posX==super.bombX+3 && p2.posY==super.bombY ){
+                        p2.life-=power;
+                    }
+                }
+
+            case 3:
+                if (super.bombX<20 && super.bombY-3>=0 ){
+                    if (p1.posX==super.bombX && p1.posY==super.bombY-3 ){
+                        p1.life-=power;
+                    }
+                    if (p2.posX==super.bombX && p2.posY==super.bombY-3 ){
+                        p2.life-=power;
+                    }
+                }
+            case 4:
+                if (super.bombX-3<20 && super.bombY>=0 ){
+                    if (p1.posX==super.bombX-3&& p1.posY==super.bombY ){
+                        p1.life-=power;
+                    }
+                    if (p2.posX==super.bombX-3 && p2.posY==super.bombY ){
+                        p2.life-=power;
+                    }
+                }
+
+
+        }
+
+    }
+
+    public void duty() {
+        destroy();
+    }
 
 }
